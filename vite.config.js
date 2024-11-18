dadbe1faa20e4589937d00dc2757d44d4ec9a88f
supertiny99 <309693441@qq.com>
2024-11-18 14:58:15 +0800

    🐎 ci: path

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,15 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/lazy-eyes-killer/',
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js',
+      },
+    },
+  },
   plugins: [vue()],
 })
